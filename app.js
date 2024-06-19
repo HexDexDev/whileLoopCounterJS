a0ace16772d28dd81eae7e3d5eeab10b92024b4d
HexDexDev <138143730+HexDexDev@users.noreply.github.com>
2024-06-19 19:30:31 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// Inicializa o contador criando a variável contador
+let contador = 1;
+
+// Loop while que continua enquanto contador for menor ou igual a 10
+while (contador <= 10) {
+    // Mostra o valor atual do contador
+    alert(contador);
+    // Incrementa o contador
+    contador++;
+}
